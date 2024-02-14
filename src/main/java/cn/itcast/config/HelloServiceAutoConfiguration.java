523d1d968908adae677c4fb40cd1d34d05520791
scy <2865298291@qq.com>
2024-02-14 21:12:30 +0800

    自定义spring-boot-starter

---
+++ src/main/java/cn/itcast/config/HelloServiceAutoConfiguration.java
@@ -0,0 +1,29 @@
+package cn.itcast.config;
+
+import cn.itcast.service.HelloService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自动配置类，用于自动配置HelloService对象
+ */
+@Configuration
+@EnableConfigurationProperties(HelloProperties.class)
+public class HelloServiceAutoConfiguration {
+
+    private HelloProperties helloProperties;
+
+    //通过构造方法注入配置属性对象HelloProperties
+    public HelloServiceAutoConfiguration(HelloProperties helloProperties) {
+        this.helloProperties = helloProperties;
+    }
+
+    //实例化HelloService并载入Spring IoC容器
+    @Bean
+    @ConditionalOnMissingBean
+    public HelloService helloService(){
+        return new HelloService(helloProperties.getName(),helloProperties.getAddress());
+    }
+}
